9c3731649d40d5f5f4bc75568c183d7234a5af37
Ryohei Endo <r-endo@studio-spoon.co.jp>
2023-05-15 11:57:46 +0900

    update

--- src/lone-metal-slime/fragment.glsl
+++ src/lone-metal-slime/fragment.glsl
@@ -68,7 +68,7 @@ float sdRoundedCylinder( vec3 p, float ra, float rb, float h ) {
 
 float cylinderRadiusNoise = 0.;
 vec3 headPos = vec3(0.);
-float bodyScale = 0.;
+float bodyScale = 1.;
 vec3 pupilPos = vec3(0.);
 
 float rand(vec2 co){
@@ -81,29 +81,29 @@ struct Intersect {
 };
 
 Intersect sdf(vec3 p) {
-  float sphere = sdCutSphere(p + vec3(0., 0.4, 0.) + headPos, 1.3, 0.4);
+  float sphere = sdCutSphere(p + vec3(0., 0.4, -0.1) + headPos, 1.4, 0.4);
   float s = .6;
 
-  float cylinder = sdRoundedCylinder(p, 1.15 + bodyScale + cylinderRadiusNoise, 0.04, .0);
+  float cylinder = sdRoundedCylinder(p, 1.2 + bodyScale + cylinderRadiusNoise, 0.08, .0);
   float body = smin(sphere, cylinder, 0.6);
-  vec3 mouthPos = rotate(p + vec3(0., -0.61, -0.27) + headPos, vec3(0, 0, -1.), PI);
+  vec3 mouthPos = rotate(p + vec3(0., -0.7, -0.42) + headPos, vec3(0, 0, -1.), PI);
   mouthPos = rotate(mouthPos, vec3(1., 0., 0.), PI * 0.42);
   float mouth = sdCappedTorus(mouthPos, vec2(sin(s), cos(s)), .8, .04);
   float final = smax(body, -mouth, .05);
   for (float i = 0.; i < 7.; i++) {
-    float rand = rand(vec2(i, 0));
-    float r = rand * 30.;
-    float progress = fract(uTime * 0.00035 * (rand + 1.) + rand);
-    vec3 pos = vec3(sin(r), .05, cos(r)) * 1.6;
+    float rand = rand(vec2(i * 10., 0));
+    float r = rand * 30. + uTime * 0.0003 * (mod(i, 2.) - .5);
+    float progress = fract(uTime * 0.0004 * mix(0.7, 1.3, rand) + i * (.4 + rand));
+    vec3 pos = vec3(sin(r), 0., cos(r)) * 1.65 * mix(0.8, 1.2, rand) + vec3(0., .05, 0.);
     float bubbleToTop = sdSphere(p + pos + vec3(0., -5., 0.) * pow(progress, 1.75), (.5 + rand) * .15 * (1.0 - smoothstep(.7, 1., progress)));
     final = smin(final, bubbleToTop, .2);
   }
 
-  float leftEye = sdRoundedCylinder(rotate(p + vec3(-0.21, -0.67, -0.9) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.06, 0.0005, 0.0005);
-  float rightEye = sdRoundedCylinder(rotate(p + vec3(0.21, -0.67, -0.9) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.06, 0.0005, 0.0005);
+  float leftEye = sdRoundedCylinder(rotate(p + vec3(-0.21, -0.76, -1.) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.06, 0.0005, 0.0005);
+  float rightEye = sdRoundedCylinder(rotate(p + vec3(0.21, -0.76, -1.) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.06, 0.0005, 0.0005);
 
-  float leftPupil = sdRoundedCylinder(rotate(p + pupilPos + vec3(-0.21, -0.67, -0.9) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.03, 0.001, 0.001);
-  float rightPupil = sdRoundedCylinder(rotate(p + pupilPos + vec3(0.21, -0.67, -0.9) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.03, 0.001, 0.001);
+  float leftPupil = sdRoundedCylinder(rotate(p + pupilPos + vec3(-0.21, -0.76, -1.) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.03, 0.001, 0.001);
+  float rightPupil = sdRoundedCylinder(rotate(p + pupilPos + vec3(0.21, -0.76, -1.) + headPos, vec3(1., 0., 0.), PI / 3.8), 0.03, 0.001, 0.001);
 
   final = min(rightPupil, min(leftPupil, min(rightEye, min(leftEye, final))));
 
@@ -111,7 +111,7 @@ Intersect sdf(vec3 p) {
   i.dist = final;
   i.color = (min(leftEye, rightEye) <= final) ? eyeColor : bodyColor;
   i.color = (min(leftPupil, rightPupil) <= final) ? pupilColor : i.color;
-  i.color = mouth - .03 <= final ? pupilColor : i.color; 
+  i.color = mouth - .03 <= final ? pupilColor : i.color;
   return i;
 }
 
@@ -134,7 +134,7 @@ void main() {
   vec3 ray = normalize(cameraSide * p.x + cameraUp * p.y + cameraDir);
 
   vec3 rayPos = cameraPos;
-  float offsetY = 0.12 * sin(uTime * 0.002);
+  float offsetY = 0.11 * sin(uTime * 0.002);
   cylinderRadiusNoise = cnoise(p * 3.2 + uTime * .0005) * 0.1;
   headPos = vec3(0.25 * sin(uTime * 0.0012), offsetY, 0.15 * cos(uTime * 0.003));
   bodyScale = offsetY * .6;
@@ -143,12 +143,12 @@ void main() {
   float tmp = 0.;
   float dist = 0.;
   vec3 color;
-  for(int i = 0; i < 128; i++){
+  for(int i = 0; i < 100; i++){
     rayPos = cameraPos + tmp * ray;
     Intersect result = sdf(rayPos);
     dist = result.dist;
     color = result.color;
-    if (dist < 0.0001 || tmp > 7.) break;
+    if (dist < 0.0001 || tmp > 6.9) break;
     tmp += dist;
   }
 
